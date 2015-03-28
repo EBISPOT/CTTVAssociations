e1cd4e0e94115905122c110a92f485f22e57c67b
vasant32 <vasant@ebi.ac.uk>
2015-03-28 19:54:25 +0000

    changes to the driver

--- src/obanminter/Driver.java
+++ src/obanminter/Driver.java
@@ -14,10 +14,10 @@ public class Driver {
       //  parser.parseFile("/Users/malone/Dropbox/CTTV/text mining work/ibd work/Clinician IBD feedback/curated_OBAN_IBD_JatinPatelreview.txt", "file:/Users/malone/Dropbox/CTTV/text%20mining%20work/ibd%20work/Clinician%20IBD%20feedback/ibd_associations_25_nov.owl");
         
         DiabParser parse = new DiabParser();
-        parse.parseDiabFile("file:/home/drashtti/Desktop/ontologies/Diabetes-Onto/diab_association_2_march.owl");
+        parse.parseDiabFile("file:/Users/vasant/DiabetesOntology/diab_association_28_march.owl");
         //parser.parseFile("/Users/malone/Dropbox/CTTV/common to rare/hpo_ORDO_141104_tab.txt", "file:/Users/malone/Dropbox/CTTV/common%20to%20rare/ordo_hpo_mappings.owl");
 
-//"file:/Users/malone/Dropbox/CTTV/text mining work/ibd work/Jatin IBD feedback/ibd_associations_27_oct.owl"
+        //"file:/Users/malone/Dropbox/CTTV/text mining work/ibd work/Jatin IBD feedback/ibd_associations_27_oct.owl"
     }
 
 
